a9d0ee04c44206769e173ae358d85a5edb99f4f9
GannaKov <zlatta2000@gmail.com>
2023-02-02 22:33:49 +0100

    home

--- screens/main/Home.js
+++ screens/main/Home.js
@@ -1,17 +1,89 @@
+import "react-native-gesture-handler"; //the libraries that are required by the stack navigator
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { PostsScreen } from "./PostsScreen";
+import { CreatePostsScreen } from "./CreatePostsScreen";
+import { ProfileScreen } from "./ProfileScreen";
+import { Feather } from "@expo/vector-icons";
+//-------------------------------------------------------
+const MainTab = createBottomTabNavigator();
+//--------------------------------------------------
 export const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+    <MainTab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+      }}
+    >
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <Feather name="grid" size={24} color="rgba(33, 33, 33, 0.8)" />
+          ),
+          headerTitle: "Публикации",
+          headerTitleAlign: "center",
+          headerRight: (focused, size, color) => (
+            <View style={{ marginRight: 10 }}>
+              <Feather
+                name="log-out"
+                size={24}
+                color="rgba(189, 189, 189, 1)"
+              />
+            </View>
+          ),
+
+          headerStyle: {
+            backgroundColor: "#FFFFFF",
+            height: 88,
+            shadowColor: "rgba(0, 0, 0, 0.3)",
+            shadowOffset: { width: 0, height: 0.5 },
+            shadowRadius: 13.5914,
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 17,
+
+            fontFamily: "Roboto-Medium",
+            fontStyle: "normal",
+            lineHeight: 22,
+          },
+          headerTintColor: "#212121",
+        }}
+        name="Posts"
+        component={PostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <View style={styles.btnAddPost}>
+              <Feather name="plus" size={24} color="#FFFFFF" />
+            </View>
+          ),
+        }}
+        name="Create Post"
+        component={CreatePostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <Feather name="user" size={24} color="rgba(33, 33, 33, 0.8)" />
+          ),
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </MainTab.Navigator>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  btnAddPost: {
+    height: 40,
+    width: 70,
+    backgroundColor: "#FF6C00",
+    borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
   },
